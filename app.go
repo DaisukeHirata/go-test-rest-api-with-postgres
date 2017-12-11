800dd913082bb1b7bba64defbaa26fa427c4528e
Daisuke Hirata <daisukihirata@gmail.com>
2017-12-11 15:57:04 +0900

    add test codes for rest api

--- app.go
+++ app.go
@@ -4,8 +4,12 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
@@ -18,33 +22,161 @@ type App struct {
 }
 
 // Initialize create a database connection
-func (a *App) Initialize(user, password, dbname, host, port string) {
-	// connectionString :=
-	// 	fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
-	// 		user,
-	// 		password,
-	// 		dbname,
-	// 		host,
-	// 		port)
-	// connectionString := fmt.Sprintf(
-	// 	"user=%s dbname=%s password=%s host=%s port=%s sslmode=disable",
-	// 	"postgres",
-	// 	"postgres",
-	// 	os.Getenv("DB_ENV_POSTGRES_PASSWORD"),
-	// os.Getenv("GOTESTRESTAPIWITHPOSTGRES_POSTGRES_1_PORT_5432_TCP_ADDR"),
-	// os.Getenv("GOTESTRESTAPIWITHPOSTGRES_POSTGRES_1_PORT_5432_TCP_PORT"),
-	// )
+func (a *App) Initialize(user, dbname, password, host, port string) {
+	connectionString := fmt.Sprintf(
+		"user=%s dbname=%s password=%s host=%s port=%s sslmode=disable",
+		user,
+		dbname,
+		password,
+		host,
+		port,
+	)
 
-	// var err error
-	// a.DB, err = sql.Open("postgres", connectionString)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < 5; i++ {
+		time.Sleep(time.Duration(i) * time.Second)
+
+		if err = a.DB.Ping(); err == nil {
+			break
+		}
+		log.Println(err)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	a.Router = mux.NewRouter()
+	a.initializeRoutes()
+
 }
 
 // Run simply starts the application.
 func (a *App) Run(addr string) {
 	log.Fatal(http.ListenAndServe(":8080", a.Router))
+}
+
+func (a *App) initializeRoutes() {
+	a.Router.HandleFunc("/products", a.getProducts).Methods("GET")
+	a.Router.HandleFunc("/product", a.createProduct).Methods("POST")
+	a.Router.HandleFunc("/product/{id:[0-9]+}", a.getProduct).Methods("GET")
+	a.Router.HandleFunc("/product/{id:[0-9]+}", a.updateProduct).Methods("PUT")
+	a.Router.HandleFunc("/product/{id:[0-9]+}", a.deleteProduct).Methods("DELETE")
+}
+
+func (a *App) getProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+		return
+	}
+
+	p := product{ID: id}
+	if err := p.getProduct(a.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Product not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, p)
+}
+
+func (a *App) getProducts(w http.ResponseWriter, r *http.Request) {
+	count, _ := strconv.Atoi(r.FormValue("count"))
+	start, _ := strconv.Atoi(r.FormValue("start"))
+
+	if count > 10 || count < 1 {
+		count = 10
+	}
+	if start < 0 {
+		start = 0
+	}
+
+	products, err := getProducts(a.DB, start, count)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, products)
+}
+
+func (a *App) createProduct(w http.ResponseWriter, r *http.Request) {
+	var p product
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	if err := p.createProduct(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, p)
+}
+
+func (a *App) updateProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+		return
+	}
+
+	var p product
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid resquest payload")
+		return
+	}
+	defer r.Body.Close()
+	p.ID = id
+
+	if err := p.updateProduct(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, p)
+}
+
+func (a *App) deleteProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Product ID")
+		return
+	}
+
+	p := product{ID: id}
+	if err := p.deleteProduct(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
--- main.go
+++ main.go
@@ -6,20 +6,18 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	_ "github.com/lib/pq"
 )
 
 var (
 	db *sql.DB
+	a  App
 )
 
 func main() {
-	_ = App{}
-
-	connInfo := fmt.Sprintf(
-		"user=%s dbname=%s password=%s host=%s port=%s sslmode=disable",
+	a = App{}
+	a.Initialize(
 		os.Getenv("DB_ENV_POSTGRES_USER"),
 		os.Getenv("DB_ENV_POSTGRES_DBNAME"),
 		os.Getenv("DB_ENV_POSTGRES_PASSWORD"),
@@ -27,37 +25,7 @@ func main() {
 		os.Getenv("DB_ENV_POSTGRES_PORT"),
 	)
 
-	var err error
-	db, err = sql.Open("postgres", connInfo)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for i := 0; i < 5; i++ {
-		time.Sleep(time.Duration(i) * time.Second)
-
-		if err = db.Ping(); err == nil {
-			break
-		}
-		log.Println(err)
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = db.Exec(
-		`create table if not exists mydata (
-			id serial primary key,
-			val integer not null
-		)`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	http.HandleFunc("/", serveIndex)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	a.Run(":8080")
 }
 
 func serveIndex(resp http.ResponseWriter, req *http.Request) {
@@ -67,12 +35,12 @@ func serveIndex(resp http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(resp, "DB_ADDR:", os.Getenv("DB_PORT_5432_TCP_ADDR"))
 	fmt.Fprintln(resp, "DB_PORT:", os.Getenv("DB_PORT_5432_TCP_PORT"))
 
-	_, err := db.Exec("insert into mydata(val) values(0)")
+	_, err := a.DB.Exec("insert into mydata(val) values(0)")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	rows, err := db.Query("select id from mydata")
+	rows, err := a.DB.Query("select id from mydata")
 	if err != nil {
 		log.Fatal(err)
 	}
